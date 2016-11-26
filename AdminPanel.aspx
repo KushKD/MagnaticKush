a5390ea6efc74564a46ac5584526ba691231b9e7
kushkd <kushkumardhawan@gmail.com>
2016-11-26 12:22:33 +0530

    Working on Admin Panel

---
+++ AdminPanel.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AdminPanel.aspx.cs" Inherits="ImagesUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <fieldset style="width: 400px; margin-left: 240px; margin-top: 90px;">
+                <legend>Example to Upload Multiple Files or images</legend>
+
+                <asp:FileUpload ID="FileUpload1" runat="server" class="multi" />
+                <br />
+                <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
+                <asp:Label ID="lblStatus" runat="server"></asp:Label>
+            </fieldset>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdminPanel.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ImagesUpload : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        StringBuilder sb = new StringBuilder();
+        try
+        {
+            HttpFileCollection hfc = Request.Files;
+            for (int i = 0; i < hfc.Count; i++)
+            {
+                HttpPostedFile hpf = hfc[i];
+                if (hpf.ContentLength > 0)
+                {
+                    hpf.SaveAs(Server.MapPath("MyFiles") + "\\" + System.IO.Path.GetFileName(hpf.FileName));
+                    sb.Append(String.Format("<br/><b>File Name: </b> {0}  <b>File Size:</b> {1}  <b>File Type:</b>  {2} uploaded successfully", hpf.FileName, hpf.ContentLength, hpf.ContentType));
+                }
+            }
+            lblStatus.ForeColor = Color.Green;
+            lblStatus.Text = sb.ToString();
+        }
+        catch (Exception ex)
+        {
+            lblStatus.ForeColor = Color.Red;
+            lblStatus.Text = "Error occured: " + ex.Message.ToString();
+        }
+
+    }
+}
